cfc5daf3164253e5e4d0a0516abf130daa0c46e0
Sohan <sthapa@hypersonicsoftware.com>
2021-10-12 15:26:59 -0700

    fixed spacing

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 func main() {
 	//using CapitalizeEveryThirdAlphanumericChar define in this package to capitalize
 	// every third alphanumeric char of a string
-	fmt.Println("using CapitalizeEveryThirdAlphanumericChar function define in the main package to" +
+	fmt.Println("using CapitalizeEveryThirdAlphanumericChar function define in the main package to " +
 		"capitalize every third alphanumeric char in a string")
 	str := CapitalizeEveryThirdAlphanumericChar("Aspiration.com")
 	fmt.Println(str)
 
-	fmt.Println("\nusing mapper package to capitalize every third alphanumeric char in a string")
+	fmt.Println("\n using mapper package to capitalize every third alphanumeric char in a string")
 	// using mapper package to capitalize every third alphanumeric char
 	s := NewSkipString(3, "Aspiration.com")
 	mapper.MapString(&s)
